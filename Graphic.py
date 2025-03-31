65d785ed7f49282cf02a8a3179880c5c7e2bd826
XHHKKK <tarelkaizpodolivok@gmail.com>
2025-03-31 22:26:49 +0300

    График

--- Graphic.py
+++ Graphic.py
@@ -1,15 +1,18 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 plt.figure()
 
 plt.xlim(-10, 10)
 plt.ylim(-10, 10)
-
+#Обозначения осей
 plt.xlabel('X')
 plt.ylabel('Y')
-
+#сетка
 plt.grid(color = 'gray', linestyle = '--', linewidth = 0.5)
 
+x=np.linspace(-10, 10, 100)
+y=x**2/2-2
+plt.plot(x,y)
 plt.title("Ось координат")
 plt.show()
-
