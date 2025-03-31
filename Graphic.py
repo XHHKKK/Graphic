802fe5c98cbfe1f3b807013811f369af261bfc48
XHHKKK <tarelkaizpodolivok@gmail.com>
2025-03-31 22:39:07 +0300

    Update Graphic.py

--- Graphic.py
+++ Graphic.py
@@ -1,18 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 plt.figure()
 
+#Обозначения осей
 plt.xlim(-10, 10)
 plt.ylim(-10, 10)
-#Обозначения осей
+
+plt.axhline(0, color='black',linewidth=0.5, ls='-')  # Горизонтальная ось
+plt.axvline(0, color='black',linewidth=0.5, ls='-')  # Вертикальная ось
+
 plt.xlabel('X')
 plt.ylabel('Y')
 #сетка
 plt.grid(color = 'gray', linestyle = '--', linewidth = 0.5)
 
-x=np.linspace(-10, 10, 100)
-y=x**2/2-2
+x=np.linspace(-5, 5, 100)
+y= x**2/2-2
 plt.plot(x,y)
 plt.title("Ось координат")
 plt.show()
