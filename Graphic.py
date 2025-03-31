9b1458c2589b8ad12fe689d1a973b335dbb2600e
XHHKKK <tarelkaizpodolivok@gmail.com>
2025-03-31 22:51:39 +0300

    Добавление формулы

--- Graphic.py
+++ Graphic.py
@@ -11,6 +11,7 @@ plt.ylim(-10, 10)
 plt.axhline(0, color='black',linewidth=0.5, ls='-')  # Горизонтальная ось
 plt.axvline(0, color='black',linewidth=0.5, ls='-')  # Вертикальная ось
 
+
 plt.xlabel('X')
 plt.ylabel('Y')
 #сетка
@@ -19,5 +20,7 @@ plt.grid(color = 'gray', linestyle = '--', linewidth = 0.5)
 x=np.linspace(-5, 5, 100)
 y= x**2/2-2
 plt.plot(x,y)
+text=plt.text(-3.65, 4.25, '$y=x^2/2-2$', fontsize=10)
 plt.title("Ось координат")
 plt.show()
+
